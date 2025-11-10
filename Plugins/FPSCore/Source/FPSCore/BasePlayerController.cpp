5d0b9b19038597ca8630a0099e6ddbb1157584e6
bread2000 <yuga.yamasaki.10041@baobab-gms.co.jp>
2025-11-10 18:38:19 +0900

    定義漏れ
        :add:InputComponentのセットアップ関数オーバーライド定義

--- Plugins/FPSCore/Source/FPSCore/BasePlayerController.cpp
+++ Plugins/FPSCore/Source/FPSCore/BasePlayerController.cpp
@@ -25,3 +25,24 @@ ABasePlayerController::~ABasePlayerController()
 	// 追加で Controller 側のバインドがあればここに書く
 	// InputComponent->BindAxis("Turn", this, &ABasePlayerController::AddYawInput);
 }
+
+
+void ABasePlayerController::SetupInputComponent()
+{
+	// 親の生成をスキップし、自前型で生成
+	InputComponent = NewObject<UBasePlayerInputComponent>(this, UBasePlayerInputComponent::StaticClass());
+	check(InputComponent);
+	InputComponent->RegisterComponent();
+
+	// PlayerController に有効化
+	PushInputComponent(InputComponent);
+
+	// 共通アクションのバインド（任意）
+	if (UBasePlayerInputComponent* Typed = Cast<UBasePlayerInputComponent>(InputComponent))
+	{
+		Typed->BindCommonActions(this);
+	}
+
+	// 追加で Controller 側のバインドがあればここに書く
+	// InputComponent->BindAxis("Turn", this, &ABasePlayerController::AddYawInput);
+}
